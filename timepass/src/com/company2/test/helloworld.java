89e65ed7afca07e4a6bbcd5f9665135b14096beb
Yogesh Naik <nike.yogesh@gmail.com>
2016-01-30 13:13:09 -0800

    company 2 commited

---
+++ timepass/src/com/company2/test/helloworld.java
@@ -0,0 +1,10 @@
+package com.company2.test;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO world");
+	}
+
+}
